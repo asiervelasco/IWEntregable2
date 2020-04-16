87ffe8fe3a2f5f6b1748ca043337c1b79af3027b
Unknown <asispinedo@opendeusto.es>
2020-04-16 20:50:05 +0200

    Ligero cambio en urls

--- empresaDjango/empresaDjango/urls.py
+++ empresaDjango/empresaDjango/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('appEmpresaDjango/', include('appEmpresaDjango.urls')),
     path('admin/', admin.site.urls),
-    path('empresaDjango/', include('appEmpresaDjango.urls'))
 ]
