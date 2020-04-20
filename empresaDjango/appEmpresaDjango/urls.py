48a90f2fa4a304422934be2ad46e48b5afdaa72d
asiervelasco <asiervelasco@opendeusto.es>
2020-04-20 11:37:37 +0200

    crear_empleado funciona

--- empresaDjango/appEmpresaDjango/urls.py
+++ empresaDjango/appEmpresaDjango/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('empleado/', views.EmpleadoListView.as_view(), name='lista_empleados'),
+     path('empleado/lista/', views.EmpleadoListView.as_view(), name='lista_empleados'),
      path('empleado/<int:pk>/', views.EmpleadoDetailView.as_view(), name='empleado'),
+     path('empleado/crear/', views.CrearEmpleado.as_view(), name='crear_empleado')
 ]
--- empresaDjango/appEmpresaDjango/views.py
+++ empresaDjango/appEmpresaDjango/views.py
@@ -10,7 +10,7 @@ def index(request):
 
 class EmpleadoListView(ListView):
     model = Empleado
-    queryset = Empleado.objects.order_by('id')
+    queryset = Empleado.objects.order_by('dni')
     template_name = "lista_empleados.html"
 
     def get_context_data(self, **kwargs):
@@ -26,3 +26,20 @@ class EmpleadoDetailView(DetailView):
         context = super(EmpleadoDetailView, self).get_context_data(**kwargs)
         context['titulo_pagina'] = 'Detalles del empleado'
         return context
+
+class CrearEmpleado(View):
+    def get(self, request, *args, **kwargs):
+        form = EmpleadoForm()
+        context = {
+            'form' :  form,
+            'titulo_pagina' : 'Crear nuevo empleado'
+        }
+        return render(request, 'crear_empleado.html', context)
+
+    def post(self, request, *args, **kwargs):
+        form = EmpleadoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_empleados')
+        return render(request, 'crear_empleado.html', {'form': form, 'titulo_pagina': 'Crear nuevo empleado'})
+
