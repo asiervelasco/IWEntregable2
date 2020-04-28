badb2bb7453bc8aa777f2c015a48dc025693357f
Unknown <asispinedo@opendeusto.es>
2020-04-28 21:04:04 +0200

    Listas reparadas

--- empresaDjango/appEmpresaDjango/urls.py
+++ empresaDjango/appEmpresaDjango/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('empleado/lista/', views.EmpleadoListView.as_view(), name='lista_empleados'),
-     path('empleado/<int:pk>/', views.EmpleadoDetailView.as_view(), name='empleado'),
+     path('empleado/lista/', views.pruebalista, name='listaempleados'),
+     path('empleado/<int:pk>/', views.EmpleadoDetailView.as_view(), name='empleadoun'),
      path('empleado/crear/', views.CrearEmpleado.as_view(), name='crear_empleado'),
-     path('tarea/lista/', views.TareaListView.as_view(), name='lista_tareas'),
-     path('tarea/<int:pk>/', views.TareaDetailView.as_view(), name='tarea'),
+     path('tarea/lista/', views.pruebalistatarea, name='listatareas'),
+     path('tarea/<int:pk>/', views.TareaDetailView.as_view(), name='tareaun'),
      path('tarea/crear/', views.CrearTarea.as_view(), name='crear_tarea'),
-     path('proyecto/lista/', views.ProyectoListView.as_view(), name='lista_proyectos'),
-     path('proyecto/<int:pk>/', views.ProyectoDetailView.as_view(), name='proyecto'),
+     path('proyecto/lista/', views.pruebalistaproyecto, name='listaproyectos'),
+     path('proyecto/<int:pk>/', views.ProyectoDetailView.as_view(), name='proyectoun'),
      path('proyecto/crear/', views.CrearProyecto.as_view(), name='crear_proyecto'),
 ]
--- empresaDjango/appEmpresaDjango/views.py
+++ empresaDjango/appEmpresaDjango/views.py
@@ -4,20 +4,19 @@ from django.views import View
 from .models import Empleado, Tarea, Proyecto
 from .forms import EmpleadoForm, TareaForm, ProyectoForm
 from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, get_list_or_404
+
 
 def index(request):
     return HttpResponse("Listado de departamentos")
+#Encargada de mostrar toda la lista de empleados, ordenados por id
+def pruebalista(request):
+    empleados =Empleado.objects.order_by('id')
+    context = {'lista_empleado': empleados,
+               'titulo_pagina':'Listado de empleados'}
+    return render(request, 'lista_empleados.html', context)
 
-class EmpleadoListView(ListView):
-    model = Empleado
-    queryset = Empleado.objects.order_by('id')
-    template_name = "lista_empleados.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(EmpleadoListView, self).get_context_data(**kwargs)
-        context['titulo_pagina'] = 'Listado de empleados'
-        return context
-
+#Encargado de mostrar un único empleado
 class EmpleadoDetailView(DetailView):
     model = Empleado
     template_name = 'empleado.html'
@@ -25,8 +24,9 @@ class EmpleadoDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super(EmpleadoDetailView, self).get_context_data(**kwargs)
         context['titulo_pagina'] = 'Detalles del empleado'
+        print(context)
         return context
-
+#Encargado de la creación de un nuevo empleado
 class CrearEmpleado(View):
     def get(self, request, *args, **kwargs):
         form = EmpleadoForm()
@@ -42,17 +42,14 @@ class CrearEmpleado(View):
             form.save()
             return redirect('lista_empleados')
         return render(request, 'crear_empleado.html', {'form': form, 'titulo_pagina': 'Crear nuevo empleado'})
+#Encargada de mostrar toda la lista de tareas, ordenadas por inicio
 
-class TareaListView(ListView):
-    model = Tarea
-    queryset = Tarea.objects.order_by('inicio')
-    template_name = "lista_tareas.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(TareaListView, self).get_context_data(**kwargs)
-        context['titulo_pagina'] = 'Listado de tareas'
-        return context
-
+def pruebalistatarea(request):
+    tareas =Tarea.objects.order_by('id')
+    context = {'lista_tareas': tareas,
+               'titulo_pagina':'Listado de Tareas'}
+    return render(request, 'lista_tareas.html', context)
+#Encargada de mostrar una tarea
 class TareaDetailView(DetailView):
     model = Tarea
     template_name = 'tarea.html'
@@ -61,7 +58,7 @@ class TareaDetailView(DetailView):
         context = super(TareaDetailView, self).get_context_data(**kwargs)
         context['titulo_pagina'] = 'Detalles de la tarea'
         return context
-
+#Encargada de la creacion de tareas
 class CrearTarea(View):
     def get(self, request, *args, **kwargs):
         form = TareaForm()
@@ -78,16 +75,14 @@ class CrearTarea(View):
             return redirect('lista_tareas')
         return render(request, 'crear_tarea.html', {'form': form, 'titulo_pagina': 'Crear nueva tarea'})
 
-class ProyectoListView(ListView):
-    model = Proyecto
-    queryset = Proyecto.objects.order_by('inicio')
-    template_name = 'lista_proyectos.html'
+#Encargada de mostrar toda la lista de proyectos, ordenados por inicio
+def pruebalistaproyecto(request):
+    empleados =Proyecto.objects.order_by('id')
+    context = {'lista_proyectos': empleados,
+               'titulo_pagina':'Listado de proyectos'}
+    return render(request, 'lista_proyectos.html', context)
 
-    def get_context_data(self, **kwargs):
-        context = super(ProyectoListView, self).get_context_data(**kwargs)
-        context['titulo_pagina'] = 'Listado de proyectos'
-        return context
-
+#Encargada de mostrar un proyecto
 class ProyectoDetailView(DetailView):
     model = Proyecto
     template_name = 'proyecto.html'
@@ -96,7 +91,7 @@ class ProyectoDetailView(DetailView):
         context = super(ProyectoDetailView, self).get_context_data(**kwargs)
         context['titulo_pagina'] = 'Detalles del proyecto'
         return context
-    
+#Encargada de la creacion de tareas
 class CrearProyecto(View):
     def get(self, request, *args, **kwargs):
         form = ProyectoForm()
